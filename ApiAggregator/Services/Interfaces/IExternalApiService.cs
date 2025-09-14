d486578c2bf287ab6083a593fc8c7d8911f373f5
AntonisKafk <antoniskafk@gmail.com>
2025-09-14 22:32:49 +0300

    Remove broken ApiAggregator folder

--- ApiAggregator/Services/Interfaces/IExternalApiService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiAggregator.Services.Interfaces
-{
-    using ApiAggregator.Models;
-    public interface IExternalApiService
-    {
-        DataSource ApiSource { get; }
-        Task<IEnumerable<AggregatedItem>> GetDataAsync(string? searchTerm = null);
-    }
-}
